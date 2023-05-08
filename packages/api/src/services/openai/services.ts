69c564ad99b20a177cde9f47d2676a88afb0375d
Andrii Khomutovskyi <andrey.khomutovskiy@quickblox.com>
2023-05-08 08:22:28 +0300

    Upgraded action points in video records

--- packages/api/src/services/openai/services.ts
+++ packages/api/src/services/openai/services.ts
@@ -2,14 +2,11 @@ import { getChatCompletion, getTranscription } from './base'
 
 export const getAudioInfo = async (fileName: string, audioFile: Buffer) => {
   const transcription = await getTranscription(fileName, audioFile)
-  const transcriptionText = transcription.reduce(
-    (res, { text }) => `${res}${text} `,
-    '',
-  )
+  const transcriptionText = transcription
+    .reduce((res, { text }) => `${res}${text} `, '')
+    .trimEnd()
 
-  const [summary = '', actions = ''] = /[\p{L}\p{N}]+/gu.test(
-    'Hello! How are you?',
-  )
+  const [summary = '', actions = ''] = /[\p{L}\p{N}]+/gu.test(transcriptionText)
     ? await Promise.all([
         getChatCompletion(
           [
@@ -22,12 +19,14 @@ export const getAudioInfo = async (fileName: string, audioFile: Buffer) => {
           [
             {
               role: 'system',
-              content: 'You can only generate text.',
+              content:
+                'If you don\'t have enough information to make an action points, display the message "There is no sufficient information to generate an action points"',
             },
             {
               role: 'user',
-              content: `Generate action points that the consultant said to do of the following passage:\n${transcriptionText}`,
+              content: `Generate action points that the consultant said to do from my dialog. Display only list without title.`,
             },
+            { role: 'user', content: `My dialog:\n${transcriptionText}` },
           ],
           { temperature: 0 },
         ),
